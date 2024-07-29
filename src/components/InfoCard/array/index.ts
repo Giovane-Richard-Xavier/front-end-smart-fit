254d572fb1b3b6ea002a8b624c321f4aef335476
Giovane <giovane@rlvtecnologia.com.br>
2024-07-28 21:08:00 -0300

    feat: implements infoCard component

---
+++ src/components/InfoCard/array/index.ts
@@ -0,0 +1,11 @@
+export const legendCard = [
+  { path: "/images/required-mask.png", label: "required" },
+  { path: "/images/recommended-mask.png", label: "recommended" },
+  { path: "/images/required-towel.png", label: "required" },
+  { path: "/images/recommended-towel.png", label: "recommended" },
+  { path: "/images/partial-fountain.png", label: "partial" },
+  { path: "/images/forbidden-fountain.png", label: "forbidden" },
+  { path: "/images/required-lockerroom.png", label: "required" },
+  { path: "/images/partial-lockerroom.png", label: "partial" },
+  { path: "/images/forbidden-lockerroom.png", label: "forbidden" },
+];
